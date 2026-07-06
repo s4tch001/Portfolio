614d0ce15f47fb68d4bb89f3d7493dfda607d44f
P <129817194+s4tch001@users.noreply.github.com>
2026-07-06 21:28:48 +0800

    security + css fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -222,6 +222,7 @@ export default function RootLayout({ children }) {
         <link
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
           rel="stylesheet"
+          integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
